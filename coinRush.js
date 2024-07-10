c26a03d80d58d68e63c71f2a4893945f996cd1d8
Using11 <aryansaran.kalla@gmail.com>
2024-07-10 08:59:09 -0600

    Update coinRush.js

--- coinRush.js
+++ coinRush.js
@@ -1,1 +1,88 @@
+var character;
+var startTime;
+var timer;
+var creatureLen = 0;
+var creatures = [];
 
+var keys = {
+  left : false,
+  right : false,
+  up : false,
+  down : false
+}
+
+var startGame = function(){
+  gameCanvas.start();
+}
+
+var updateGame = function(){
+  gameCanvas.clear();
+  if(keys.left){
+    
+  }
+  if(keys.right){
+    
+  }
+  if(keys.up){
+    
+  }
+  if(keys.down){
+    
+  }
+  paint();
+}
+
+var gameCanvas = {
+  canvas : document.getElementById("CoinRush"),
+  start : function(){
+    this.context = canvas.getContext("2d");
+    this.animation = paint();
+    updateGame();
+    this.addEventListener("keydown", function(event){
+      gameCanvas.key = event.key
+      if(gameCanvas.key === "ArrowLeft"){
+        keys.left = true;
+      }
+      if(gameCanvas.key === "ArrowRight"){
+        keys.right = true;
+      }
+      if(gameCanvas.key === "ArrowUp"){
+        keys.up = true;
+      }
+      if(gameCanvas.key === "ArrowDown"){
+        keys.down = true;
+      }
+    });    
+    this.addEventListener("keyup", function(event){
+      gameCanvas.key = event.key
+      if(gameCanvas.key === "ArrowLeft"){
+        keys.left = false;
+      }
+      if(gameCanvas.key === "ArrowRight"){
+        keys.right = false;
+      }
+      if(gameCanvas.key === "ArrowUp"){
+        keys.up = false;
+      }
+      if(gameCanvas.key === "ArrowDown"){
+        keys.down = false;
+      }
+    });
+  },
+  clear : function(){
+    this.context.clearRect(0,0,canvas.width,canvas.height);
+  }
+};
+
+class component{
+  constructor(){
+    
+  }
+}
+
+
+function paint(){
+  setTimeout(() => {
+    requestAnimationFrame(updateGame);
+  }, 1000 / 30);
+}
