f2556deea7d50a5201f455c4b93dede9d903f7cf
Using11 <aryansaran.kalla@gmail.com>
2024-07-10 09:30:03 -0600

    Update coinRush.js

--- coinRush.js
+++ coinRush.js
@@ -79,7 +79,7 @@ var gameCanvas = {
     });
   },
   clear : function(){
-    this.context.clearRect(0,0,canvas.width,canvas.height);
+    this.context.clearRect(0,0,this.canvas.width,this.canvas.height);
   }
 };
 
