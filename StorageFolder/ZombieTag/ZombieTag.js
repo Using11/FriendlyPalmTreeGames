52222ba822170aea702b48904b6cbecfc991d109
Using11 <aryansaran.kalla@gmail.com>
2024-07-16 17:03:49 -0600

    Update ZombieTag.js

--- StorageFolder/ZombieTag/ZombieTag.js
+++ StorageFolder/ZombieTag/ZombieTag.js
@@ -1,1 +1,13 @@
+var character;
+var score;
+var time;
 
+var gameCanvas = {
+  canvas : document.getElementById("ZombieTag"),
+  start : function(){
+    
+  },
+  clear : function(){
+    
+  }
+}
