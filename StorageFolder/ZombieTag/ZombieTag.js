86d64b1695df1c33d2daec60a3017314879e904e
Using11 <aryansaran.kalla@gmail.com>
2024-07-16 17:21:27 -0600

    Update ZombieTag.js

--- StorageFolder/ZombieTag/ZombieTag.js
+++ StorageFolder/ZombieTag/ZombieTag.js
@@ -2,12 +2,101 @@ var character;
 var score;
 var time;
 
+var keys = {
+  left : false,
+  right : false,
+  up : false,
+  down : false
+}
+
+var startGame = function(){
+  character = new component(100,100,20,20,"red");
+  gameCanvas.start();
+}
+
+var updateGame = function(){
+  gameCanvas.clear();
+  character.update();
+  paint();
+}
+
 var gameCanvas = {
   canvas : document.getElementById("ZombieTag"),
   start : function(){
-    
+    this.context = this.canvas.getContext("2d");
+    this.animation = paint();
+    updateGame();
+    window.addEventListener("keydown", function(event){
+      gameCanvas.key = event.key
+      if(gameCanvas.key === "ArrowLeft"){
+        keys.left = true;
+      }
+      if(gameCanvas.key === "ArrowRight"){
+        keys.right = true;
+      }
+      if(gameCanvas.key === "ArrowUp"){
+        keys.up = true;
+      }
+      if(gameCanvas.key === "ArrowDown"){
+        keys.down = true;
+      }
+    });    
+    window.addEventListener("keyup", function(event){
+      gameCanvas.key = event.key
+      if(gameCanvas.key === "ArrowLeft"){
+        keys.left = false;
+      }
+      if(gameCanvas.key === "ArrowRight"){
+        keys.right = false;
+      }
+      if(gameCanvas.key === "ArrowUp"){
+        keys.up = false;
+      }
+      if(gameCanvas.key === "ArrowDown"){
+        keys.down = false;
+      }
+    });
+    window.addEventListener("keydown", function(e) {
+      if(["ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(e.code) > -1) {
+        e.preventDefault();
+      }
+    }, false);
   },
   clear : function(){
-    
+    this.context.clearRect(0,0,this.canvas.width,this.canvas.height);
   }
+}
+
+class component{
+  constructor(x,y,width,height,color){
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    this.update = function(){
+      this.build();
+    }
+  }
+  build(){
+    var context = gameCanvas.canvas.getContext("2d");
+    context.fillStyle = this.color;
+    context.strokeStyle = "black";
+    context.lineWidth = 2;
+    context.fillRect(this.x,this.y,this.width,this.height);
+    context.strokeRect(this.x,this.y,this.width,this.height);
+  }
+}
+
+class zombie extends component{
+  constructor(x,y,width,height,color,speed){
+    super(x,y,width,height,color); 
+    this.speed = speed;
+  }
+}
+
+function paint(){
+  setTimeout(() => {
+    requestAnimationFrame(updateGame);
+  }, 1000 / 30);
 }
