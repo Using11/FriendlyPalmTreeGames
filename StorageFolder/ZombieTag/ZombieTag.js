d685912495ed51f3254525346c93ba457cb6193a
Using11 <aryansaran.kalla@gmail.com>
2024-07-19 13:23:58 -0600

    Update ZombieTag.js

--- StorageFolder/ZombieTag/ZombieTag.js
+++ StorageFolder/ZombieTag/ZombieTag.js
@@ -194,8 +194,144 @@ function paint(){
   }, 1000 / 30);
 }
 
+var Canvas = document.getElementById("ZombieTag");
+var CanvasRect = Canvas.getBoundingClientRect();
+var CanvasContext = Canvas.getContext("2d");
+
 function intro(){
-  startGame();
+  CanvasContext.strokeStyle = "black";
+  CanvasContext.fillStyle = "white";
+  function homepage(){
+    CanvasContext.clearRect(0,0,Canvas.width,Canvas.height);
+    function homepageAdvance(event){
+      if(event.clientX - CanvasRect.left >= 45 && event.clientX - CanvasRect.left <= 285 && event.clientY - CanvasRect.top >= 150 && event.clientY - CanvasRect.top <= 310){
+        //timer = 20;
+        startGame();
+      }
+      else if(event.clientX - CanvasRect.left >= 330 && event.clientX - CanvasRect.left <= 570 && event.clientY - CanvasRect.top >= 150 && event.clientY - CanvasRect.top <= 310){
+        //timer = 60;
+        startGame();
+      }
+      else if(event.clientX - CanvasRect.left >= 615 && event.clientX - CanvasRect.left <= 855 && event.clientY - CanvasRect.top >= 150 && event.clientY - CanvasRect.top <= 310){
+        //timer = 180;
+        startGame();
+      }
+      else if(event.clientX - CanvasRect.left >= 45 && event.clientX - CanvasRect.left <= 285 && event.clientY - CanvasRect.top >= 355 && event.clientY - CanvasRect.top <= 515){
+        credits();
+      }
+      else if(event.clientX - CanvasRect.left >= 330 && event.clientX - CanvasRect.left <= 570 && event.clientY - CanvasRect.top >= 355 && event.clientY - CanvasRect.top <= 515){
+        instructions();
+      }
+      else if(event.clientX - CanvasRect.left >= 615 && event.clientX - CanvasRect.left <= 855 && event.clientY - CanvasRect.top >= 355 && event.clientY - CanvasRect.top <= 515){
+        leaderboard();
+      }
+      Canvas.removeEventListener("click",homepageAdvance);
+    }
+    CanvasContext.strokeRect(45,150,240,160);
+    CanvasContext.strokeRect(330,150,240,160);
+    CanvasContext.strokeRect(615,150,240,160);
+    CanvasContext.strokeRect(45,355,240,160);
+    CanvasContext.strokeRect(330,355,240,160);
+    CanvasContext.strokeRect(615,355,240,160);
+    CanvasContext.fillStyle = "black";
+    CanvasContext.textAlign = "center";
+    CanvasContext.font = "45px Arial";
+    CanvasContext.fillText("Zombie Tag",450,75);
+    CanvasContext.textBaseline = "middle";
+    CanvasContext.fillText("?",165,230);
+    CanvasContext.fillText("?",450,230);
+    CanvasContext.fillText("?",735,230);
+    CanvasContext.fillText("Credits",165,435);
+    CanvasContext.fillText("Instructions",450,435);
+    CanvasContext.fillText("Leader",735,413);
+    CanvasContext.fillText("board",735,457);
+    Canvas.addEventListener("click",homepageAdvance);
+  }
+  function credits(){
+    CanvasContext.clearRect(0,0,Canvas.width,Canvas.height);
+    function creditsAdvance(event){
+    if(event.clientX - CanvasRect.left >= 330 && event.clientX - CanvasRect.left <= 570 && event.clientY - CanvasRect.top >= 395 && event.clientY - CanvasRect.top <= 555){
+        homepage();
+      }
+      Canvas.removeEventListener("click", creditsAdvance);
+    }
+    CanvasContext.strokeRect(330,395,240,160);
+    CanvasContext.fillStyle = "black";
+    CanvasContext.textAlign = "center";
+    CanvasContext.fillText("Credits:",450,75);
+    CanvasContext.textBaseline = "middle";
+    CanvasContext.fillText("Back",450,475);
+    CanvasContext.fillText("Using11 (creator / dev)",450,175);
+    CanvasContext.fillText("Github (platform)",450,250);
+    Canvas.addEventListener("click", creditsAdvance);
+  }
+  function instructions(){
+    CanvasContext.clearRect(0,0,Canvas.width,Canvas.height);
+    function instructionsAdvance(event){
+      if(event.clientX - CanvasRect.left >= 330 && event.clientX - CanvasRect.left <= 570 && event.clientY - CanvasRect.top >= 395 && event.clientY - CanvasRect.top <= 555){
+        homepage();
+      }
+      Canvas.removeEventListener("click", instructionsAdvance);
+    }
+    CanvasContext.strokeRect(330,395,240,160);
+    CanvasContext.fillStyle = "black";
+    CanvasContext.textAlign = "center";
+    CanvasContext.font = "45px Arial";
+    CanvasContext.fillText("Instructions:",450,75);
+    CanvasContext.textBaseline = "middle";
+    CanvasContext.fillText("Back",450,475);
+    CanvasContext.font = "30px Arial";
+    CanvasContext.fillText("Use the arrow keys to move around your character",450,150);
+    CanvasContext.fillText("Run away from the zombies that chase you",450,225);
+    CanvasContext.fillText("More zombies that are stronger will spawn the longer you survive",450,300);
+    CanvasContext.fillText("Survive long enough to be featured on the leaderboard!",450,375);
+    Canvas.addEventListener("click", instructionsAdvance);
+  }
+  function leaderboard(){
+    CanvasContext.clearRect(0,0,Canvas.width,Canvas.height);
+    function leaderboardAdvance(event){
+      if(event.clientX - CanvasRect.left >= 330 && event.clientX - CanvasRect.left <= 570 && event.clientY - CanvasRect.top >= 395 && event.clientY - CanvasRect.top <= 555){
+        homepage();
+      }
+      Canvas.removeEventListener("click", leaderboardAdvance);
+    }
+    CanvasContext.strokeRect(330,395,240,160);
+    CanvasContext.fillStyle = "black";
+    CanvasContext.textAlign = "center";
+    CanvasContext.font = "45px Arial";
+    CanvasContext.fillText("Leaderboard:",450,75);
+    CanvasContext.textBaseline = "middle";
+    CanvasContext.fillText("Back",450,475);
+    CanvasContext.fillText("Coming soon...",450,175);
+    CanvasContext.fillText("Submit your score to qualify!",450,250);
+    Canvas.addEventListener("click", leaderboardAdvance);
+  }
+  homepage();
+}
+
+var endgame = function(){
+  character.xMove = 0;
+  character.yMove = 0;
+  scoreHTML.remove();
+  timerHTML.remove();
+  CanvasContext.clearRect(0,0,Canvas.width,Canvas.height);
+  function replay(event){
+    if(event.clientX - CanvasRect.left >= 330 && event.clientX - CanvasRect.left <= 570 && event.clientY - CanvasRect.top >= 395 && event.clientY - CanvasRect.top <= 555){
+      intro();
+    }
+    Canvas.removeEventListener("click", replay);
+  }
+  CanvasContext.strokeStyle = "black";
+  CanvasContext.fillStyle = "white";
+  CanvasContext.strokeRect(330,395,240,160);
+  CanvasContext.textAlgin = "center";
+  CanvasContext.fillStyle = "black";
+  CanvasContext.font = "45px Arial";
+  CanvasContext.fillText("You collected " + coinsCollected + " coins in " + timer + " seconds",450,75);
+  CanvasContext.fillText("Your final score is: " + score,450,150);
+  CanvasContext.textBaseline = "center";
+  CanvasContext.fillText("Back",450,475);
+  Canvas.addEventListener("click", replay);
 }
 
 intro();
